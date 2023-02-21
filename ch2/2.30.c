518e8e9f1ba1cf61082cc89e6433023ccd85779d
humbornjo <humbornjo@gmail.com>
2023-02-22 03:13:11 +0900

    230223

---
+++ ch2/2.30.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int tadd_ok(int x, int y){
+	int sum = x+y;
+	int neg_over = x < 0 && y < 0 && sum > 0;
+	int pos_over = x > 0 && y > 0 && sum < 0;
+	return !neg_over && !pos_over;
+}	
